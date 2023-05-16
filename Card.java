6d11bf9e1432aa021b3fb7c7a0dff86feff14fdd
NUR AISYAH NABILA <108205247+PennCakeSb@users.noreply.github.com>
2023-05-16 10:47:15 +0800

    assign value fpr rank

--- Card.java
+++ Card.java
@@ -1,10 +1,45 @@
 public class Card {
     private String rank;
     private String suit;
+    private int value;
 
     public Card(String rank, String suit) {
         this.rank = rank;
         this.suit = suit;
+        this.value = assignValue(rank);
+    }
+
+    public int assignValue(String rank) {
+        switch (rank) {
+            case "2":
+                return 1;
+            case "3":
+                return 2;
+            case "4":
+                return 3;
+            case "5":
+                return 4;
+            case "6":
+                return 5;
+            case "7":
+                return 6;
+            case "8":
+                return 7;
+            case "9":
+                return 8;
+            case "X":
+                return 9;
+            case "J":
+                return 10;
+            case "Q":
+                return 11;
+            case "K":
+                return 12;
+            case "A":
+                return 13;
+            default:
+                return 0;
+        }
     }
 
     public String getRank() {
@@ -13,6 +48,10 @@ public class Card {
 
     public String getSuit() {
         return suit;
+    }
+
+    public int getValue() {
+        return value;
     }
 
     @Override
