f87b9a177b0987503f41527535f18b823a5f6923
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2023-05-16 15:56:47 +0800

    Update Deck.java
    Changed the way the cards are created and moved some functions from the previous GoBoom file to here.

--- Deck.java
+++ Deck.java
@@ -1,25 +1,75 @@
-import java.util.ArrayList;
+
 import java.util.Collections;
+import java.util.ArrayList;
 
-public class Deck {
-    private ArrayList<Card> cards;
+public class Deck{
+    private ArrayList<Card> deck=new ArrayList<>();
+    private Card leadCard;
 
-    public Deck(){
-        String[] ranks = {"A", "K", "Q", "J", "10", "9", "8", 
-                          "7", "6", "5", "4", "3", "2"};
-        String[] suits = {"c", "d", "h", "s"};
-        cards = new ArrayList<Card>();
-
-        for (String suit : suits){
-            for (String rank : ranks){
-                cards.add(new Card(rank,suit));
-            }
-        }
-
-        Collections.shuffle(cards);
+    //constructor
+    public Deck(ArrayList<Card> deck){
+        this.deck=deck;
     }
 
-    public ArrayList<Card> getCards(){
-        return cards;
+    // Generate Cards
+    public void generateCards(){
+        char[] numbers={'2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K', 'A'};
+        char[] suits={'d', 'c', 'h', 's'};
+        // char[] suits = {'♦', '♣', '♥', '♠'}; 
+        
+        for(char suit:suits){
+            for(int i=0;i<13;i++){
+                Card x=new Card(i+1, numbers[i], suit);
+                deck.add(x);
+            }
+        }
+    }
+
+    // Return the whole deck. 
+    public ArrayList<Card> getDeck(){
+        return deck;
+    }
+
+    // Print whole deck
+    public void printDeck(){
+        ArrayList<String> strDeck=new ArrayList<String>();
+
+        for(Card card:this.getDeck()){
+            char suit=card.getSuit();
+            char number=card.getNumber();
+            String strCard=Character.toString(suit) + Character.toString(number);
+            strDeck.add(strCard);
+        }
+        System.out.println(strDeck.toString());
+    }
+
+    // Return the top card from the deck.
+    public Card getLeadCard(){
+        return deck.get(0);
+    }
+
+    // Shuffle the whole deck. 
+    public void shuffleDeck(){
+        Collections.shuffle(deck);
+    }
+
+    // Get card from the deck.
+    public Card getCardAtIndex(int n){
+        return deck.get(n);
+    }
+
+    // Remove card from the deck.
+    public Card removeCardAtIndex(int n){
+        return deck.remove(n);
+    }
+
+    // Add card to deck
+    public void addCard(Card card){
+        deck.add(card);
+    }
+
+    // Set lead card
+    public void setLeadCard(Card card){
+        this.leadCard=card;
     }
 }
