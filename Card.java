0bcce24548afe4aadb5c9800df05d0366991383c
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2023-05-16 15:55:02 +0800

    Update Card.java
    Moved some functions from the previous GoBoom file to here

--- Card.java
+++ Card.java
@@ -1,61 +1,37 @@
-public class Card {
-    private String rank;
-    private String suit;
+
+public class Card{
     private int value;
+    private char number, suit;
 
-    public Card(String rank, String suit) {
-        this.rank = rank;
-        this.suit = suit;
-        this.value = assignValue(rank);
+    public Card(int value, char number, char suits){
+        this.number=number;
+        this.suit=suits;
+        this.value=value;
     }
 
-    public int assignValue(String rank) {
-        switch (rank) {
-            case "2":
-                return 1;
-            case "3":
-                return 2;
-            case "4":
-                return 3;
-            case "5":
-                return 4;
-            case "6":
-                return 5;
-            case "7":
-                return 6;
-            case "8":
-                return 7;
-            case "9":
-                return 8;
-            case "X":
-                return 9;
-            case "J":
-                return 10;
-            case "Q":
-                return 11;
-            case "K":
-                return 12;
-            case "A":
-                return 13;
-            default:
-                return 0;
-        }
+    public void setValue(int n){
+        this.value=n;
+    };
+    public void setNumber(char x){
+        this.number=x;
+    };
+    public void setSuit(char x){
+        this.suit=x;
+    };
+    public int getValue(){
+        return value;
     }
-
-    public String getRank() {
-        return rank;
+    public char getNumber(){
+        return number;
     }
-
-    public String getSuit() {
+    public char getSuit(){
         return suit;
     }
 
-    public int getValue() {
-        return value;
+    public void printCurrentCardDetailed(){
+        System.out.println("value is : " + this.getValue() + ", number is: " + this.getNumber() + ", suit is  : " + this.getSuit());
     }
-
-    @Override
-    public String toString() {
-        return suit + rank;
+    public void printCurrentCard(){
+        System.out.printf("%c%c", this.getNumber(), this.getSuit());
     }
 }
