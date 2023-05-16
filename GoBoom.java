a1836dc356f662b113ace4c06716e6bcf2ac12a7
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2023-05-16 15:53:36 +0800

    Update GoBoom.java
    Minor changes from the previous version

--- GoBoom.java
+++ GoBoom.java
@@ -1,137 +1,100 @@
+
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class GoBoom {
-    private static final String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "X", "J", "Q", "K", "A"};
-    private static final String[] players = {"Player1", "Player2", "Player3", "Player4"};
-    private static final int numOfPlayers = 4;
-    private static final int numOfCards = 7;
 
     public static void main(String[] args) {
-        // create a new deck
-        ArrayList<Card> deck = new ArrayList<>();
+        
+        // //   All cards should be faced up to facilitate checking and Start a new game with randomized 52 cards.
 
-        // create cards with ranks and suits
-        String[] suits = {"h", "d", "c", "s"};
-        for (String suit : suits) {
-            for (String rank : ranks) {
-                    Card card = new Card(rank, suit);
-                    deck.add(card);    
-            }
-        }
+        // Generating the main deck of card
+        ArrayList<Card> deck=new ArrayList<>();
+        Deck mainDeck = new Deck(deck);
+        mainDeck.generateCards();
 
-        // shuffle the deck
-        Collections.shuffle(deck);
+        // Shuffling the generated deck of cards
+        mainDeck.shuffleDeck();
 
-        // get the lead card
-        Card leadCard = deck.remove(0);
+        // Deal cards to four players
+        int numOfPlayers = 4;
+        int numOfCards = 7;
 
-        // determine the first player based on the lead card
-        int firstPlayerIndex = getFirstPlayerIndex(leadCard);
+        // Creating an array of players
+        Player[] players=new Player[4];
 
-        //create players and deal cards
-        ArrayList<Player> playerList = new ArrayList<>();
+
+
+        // // The first card in the deck is the first lead card and is placed at the center.
+
+        // Create a 'center' Array List and insert the lead card from the deck.
+        ArrayList<Card> centerDeck=new ArrayList<>();
+        Deck center=new Deck(centerDeck);
+
+        Card leadMainDeck=mainDeck.removeCardAtIndex(0); // lead card is removed from the deck
+        center.addCard(leadMainDeck); // removed lead card added to the center
+
+
+
+        // //   The first lead card determines the first player and Deal 7 cards to each of the 4 players.
+
+        // Adding cards to the new player deck
         for (int i = 0; i < numOfPlayers; i++) {
-            int currentPlayerIndex = (firstPlayerIndex + i) % numOfPlayers;
+
             ArrayList<Card> playerCards = new ArrayList<>();
-            for (int j = 0; j < numOfCards; j++) {
-                Card card = deck.remove(0); //lead card is removed from the deck
-                playerCards.add(card);
+            char[] ranks = {'A', '5', '9', 'K', '2', '6', 'X', '3', '7', 'J', '4', '8', 'Q'};
+            char leadCardNum=center.getLeadCard().getNumber();
+
+            // Add cards to the playersCard Array List.
+            for (int j = 0; j < numOfCards; j++){
+                Card cardToBeAdded=mainDeck.getCardAtIndex(0);
+                playerCards.add(cardToBeAdded);
+                mainDeck.removeCardAtIndex(0);
             }
-            Player player = new Player(players[currentPlayerIndex], playerCards);
-            playerList.add(player);
-        }
 
-        // print the players' cards
-        printPlayerCards(playerList);
+            // Set each players' turn.
+            for(int k=0; k<ranks.length;k++){
+                if(leadCardNum==ranks[k]){
+                    int currentPlayerIndex = (k + i) % numOfPlayers;
 
-        // print the lead card
-        System.out.println("Center: [" + leadCard + "]");
-
-        // print the remaining deck
-        System.out.print("Deck: ");
-        printCards(deck);
-
-        // print the score
-        System.out.println("Score: " + getPlayerScores(playerList));
-
-        // print the first player
-        System.out.println("Turn: " + "Player"+firstPlayerIndex);
-
-        // checking if a value corresponds to the rank assigned to each card
-        //    String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "X", "J", "Q", "K", "A"};
-
-        //     for (String rank : ranks) {
-        //         Card card = new Card(rank, "s");
-        //         int value = card.assignValue(rank);
-        //         if (value == 0) {
-        //             System.out.println(rank + " does not have a value.");
-        //         } else {
-        //             System.out.println(rank + " has a value of " + value + ".");
-        //         }
-        //     }
-        
-
-        //    // print the current turn
-        //    System.out.println("Turn: " + getCurrentTurn(playerList, firstPlayerIndex));
-    }
-
-    private static int getFirstPlayerIndex(Card leadCard) {
-        String rank = leadCard.getRank();
-        if (rank.equals("A") || rank.equals("5") || rank.equals("9") || rank.equals("K")) {
-            return 1; // Player1 goes first
-        } else if (rank.equals("2") || rank.equals("6") || rank.equals("10")) {
-            return 2; // Player2 goes first
-        } else if (rank.equals("3") || rank.equals("7") || rank.equals("J")) {
-            return 3; // Player3 goes first
-        } else if (rank.equals("4") || rank.equals("8") || rank.equals("Q")) {
-            return 4; // Player4 goes first
-        } else {
-            return 0; // Default to Player1
-        }
-    }
-
-    private static void printPlayerCards(ArrayList<Player> playerList) {
-        for (String playerName : players) {
-            for (Player player : playerList) {
-                if (player.getName().equals(playerName)) {
-                    System.out.print(player.getName() + ": ");
-                    printCards(player.getCards());
-                    break;
+                    // Create players.
+                    players[i] = new Player(i+1, playerCards, currentPlayerIndex+1);
                 }
             }
         }
+
+        // Display cards, deck, center deck, score and turn
+        int turn=0;
+        printRoundsDetails(numOfPlayers, players, center, mainDeck, turn);
     }
 
-    private static void printCards(ArrayList<Card> cards) {
-        StringBuilder cardBuilder = new StringBuilder();
-        for (Card card : cards) {
-            cardBuilder.append(card).append(", ");
-        }
-        String cardList = cardBuilder.toString().trim();
-        if (!cardList.isEmpty()) {
-            cardList = cardList.substring(0, cardList.length() - 1);
-        }
-        System.out.println("[" + cardList + "]");
-    }
+    public static void printRoundsDetails(int numOfPlayers, Player[] players, Deck center, Deck mainDeck, int turn){
+         // print the players' cards
+         for(int i=0; i<numOfPlayers; i++){
+             System.out.print("Player " + players[i].getPlayerNum() + ": ");
+             players[i].printDeck();
+         }
 
-    private static String getPlayerScores(ArrayList<Player> playerList) {
-        StringBuilder scoreBuilder = new StringBuilder();
-        
-        // Append the scores of players in the order of Player 1 to Player 4
-        for (String playerName : players) {
-            for (Player player : playerList) {
-                if (player.getName().equals(playerName)) {
-                    scoreBuilder.append(player.getName()).append(" = ").append(player.getScore()).append(" | ");
-                    break;
-                }
+         // print the lead card
+         System.out.print("Center: ");
+         center.printDeck();
+ 
+         // print the remaining deck
+         System.out.print("Deck: ");
+         mainDeck.printDeck();
+ 
+         // print the score
+         System.out.print("Score: ");
+         for(int i=0; i<numOfPlayers; i++){
+            System.out.print("Player " + players[i].getPlayerNum() + " = " + players[i].getPlayerScore());
+            if(i!=3){ // there should be 
+                System.out.print(" | ");
             }
-        }
-        
-        String scores = scoreBuilder.toString().trim();
-        if (!scores.isEmpty()) {
-            scores = scores.substring(0, scores.length() - 1);
-        }
-        return scores;
+            else{
+                System.out.println();
+            }
+         }
+ 
+         // print current player's turn
+         System.out.println("Turn: " + turn);
     }
 }
