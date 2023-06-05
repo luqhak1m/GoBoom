f7693cd6eeef32ec0580e1191113b5b34a8a6409
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2023-06-05 22:51:08 +0800

    minor update from part 1
    code is a bit cleaner now

--- GoBoom.java
+++ GoBoom.java
@@ -6,94 +6,72 @@ public class GoBoom {
     public static void main(String[] args) {
         int numOfPlayers = 4; // Four Players.
         int numOfCards = 7; // 7 Cards for each player.
-        int turn=1; // Number of turns.
+        int trick=1; // Number of turns.
 
         Turn gameTurn=new Turn(numOfPlayers, numOfCards); // New Turn object to handle the gameplay.
-
         Scanner input = new Scanner(System.in);
 
-        for(Player player:gameTurn.getPlayers()){ // Set the lead player.
-            if(player.getPlayerTurn()==1){
-                gameTurn.setCurrentLeadPlayer(player);
-            }
-        }
-        
+        gameTurn.Welcome();
         // Game starts.
-        while(turn>0){ // This will be the tricks.
+        while(trick>0){ // This will be the tricks.
 
-            if(!gameTurn.getCenter().emptyDeck()){ // Set the Lead Card and the Highest Value Card (at this stage of the game, both are the same).
-                gameTurn.setCurrentLeadCard(gameTurn.getCenter().getLeadCard());
-                gameTurn.setHighestValCard(gameTurn.getCenter().getLeadCard());
+            Deck centerDeck=gameTurn.center;
+            gameTurn.setLeadPlayer(); // Set the lead player
+
+            if(!centerDeck.emptyDeck()){ // Set the Lead Card and the Highest Value Card (at this stage of the game, both are the same).
+                Card centerLeadCard=centerDeck.getLeadCard();
+                gameTurn.setHighestValCard(centerLeadCard);
             }
 
-            if (turn==1){
-                Welcome();
-            }
-            
+            // // This is the rounds.
+
             for(int i=0; i<numOfPlayers; i++){ // For each trick's round.
+                gameTurn.setTrickNum(trick);
+                gameTurn.setRoundNum(i+1);
+                if(gameTurn.getMode()==1||gameTurn.getMode()==2){
+                    break;
+                }
+                int currentRoundNumber=gameTurn.getRoundNum();
                 for(Player player:gameTurn.getPlayers()){ // For each player in the players array.
-                    if(player.getPlayerTurn()==i+1){ // Get player on current turn.
-                        gameTurn.turn(i, input, player, gameTurn.getCenter(), gameTurn.getMainDeck(), turn, numOfPlayers, gameTurn.getPlayers());
-                        
-
-                        if(gameTurn.getMode()==1){ // If player types 's'.
-                            System.out.println("Restarting game.");
-                            break;
-                        }
-
-
-                        if(gameTurn.getMode()==2){ // If player types 'x'.
-                            System.out.println("Thank you for playing!");
-                            break;
-                        }
+                    if(player.getPlayerTurn()==currentRoundNumber){ // Get player on current round.
+                        gameTurn.turn(input, player, numOfPlayers);
                     }
                 }  
-                if(gameTurn.getMode()==2 || gameTurn.getMode()==1){ // Break from the for loop.
+            } 
+            
+            // // End of 4 Rounds.
+
+            if(gameTurn.getMode()==1||gameTurn.getMode()==2){
+                if(gameTurn.getMode()==1){
+                    System.out.println("Starting a new game...");
+                    gameTurn=new Turn(numOfPlayers, numOfCards); // create new game
+                    trick=1;
+                    gameTurn.setMode(0);
+                    continue;
+                }
+                else{
+                    System.out.println("Thank You for playing!");
                     break;
-                }  
-            }
-            if(gameTurn.getMode()==1){ // If player resets the game.
-                gameTurn=new Turn(numOfPlayers, numOfCards); // Create a new Turn object on the Previous Turn object.
+                }
             }
 
+            gameTurn.getCurrentLeadPlayer().addPlayersScore(); //Add trick winner's score
+            System.out.println();System.out.println("*** Player " + gameTurn.getCurrentLeadPlayer().getPlayerNum() + " wins Trick #" + gameTurn.getTrickNum() + " ***"); // Show which player wins the round.
+            gameTurn.getCenter().getDeck().clear(); // Clear the center deck for new trick.
 
-            else if(gameTurn.getMode()==2){ // If player exits the game.
-                break; // Break the while loop.
-            }
-
-
-            else if(gameTurn.getMode()==0){ // After four (4) rounds has ended.
-                System.out.println();System.out.println("*** Player " + gameTurn.getCurrentLeadPlayer().getPlayerNum() + " wins Trick #" + turn + " ***"); // Show which player wins the round.
-                gameTurn.getCenter().getDeck().clear(); // Clear the deck for new trick.
-    
-                // Change the remaining user's turn based on the previous trick winner.
-                for(int l=0; l<numOfPlayers; l++){
-                    int currentPlayerNumber=(gameTurn.getCurrentLeadPlayer().getPlayerNum()+l)%4;
-                    for(Player player:gameTurn.getPlayers()){
-                        if(currentPlayerNumber==0){ // Because 4%4==0. We are aiming for 4 not 0.
-                            currentPlayerNumber=4;
-                        }
-                        if(player.getPlayerNum()==currentPlayerNumber){ 
-                            player.setPlayerTurn(l+1); // Each player's turn is now reassigned
-                        }
+            // Change the remaining user's turn based on the previous trick winner.
+            for(int l=0; l<numOfPlayers; l++){
+                int currentPlayerNumber=(gameTurn.getCurrentLeadPlayer().getPlayerNum()+l)%4;
+                for(Player player:gameTurn.getPlayers()){
+                    if(currentPlayerNumber==0){ // Because 4%4==0. We are aiming for 4 not 0.
+                        currentPlayerNumber=4;
+                    }
+                    if(player.getPlayerNum()==currentPlayerNumber){ 
+                        player.setPlayerTurn(l+1); // Each player's turn is now reassigned
                     }
                 }
-                turn++; // Add turn count.
             }
-
-        }
-    }
-
-    public static void Welcome(){
-        System.out.println("-- Welcome to Go Boom --");
-        System.out.print("Press 'c' to see commands: ");
-        Scanner input = new Scanner(System.in);
-        String command = input.nextLine();
-
-        if(command.equals("c")|| command.equals("C")){
-            Turn.Menu();
-            System.out.print("Press any key to play: ");
-            command = input.nextLine();
-        }
+            trick++; // Add turn count.
+        }   
     }
 }
