dae3f23f1fc09c1a62d3abc46a4738d162029c07
Luqman Hakim <123793185+luqhak1m@users.noreply.github.com>
2023-06-12 21:41:02 +0800

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,10 +1,11 @@
 
 import java.util.HashMap;
 
-public class Card{
+public class Card implements Comparable<Card>{
     private int value;
     private char number, suit;
     private String initial;
+    static HashMap<Character, Integer> cardScore;
 
     public Card(int value, char number, char suits, HashMap<Integer, String> cardInitial){
         this.number=number;
@@ -13,6 +14,25 @@ public class Card{
         
         String tempCard=String.valueOf(suits)+cardInitial.get(value);
         this.initial=tempCard;
+
+        cardScore=new HashMap<>();
+
+        char[] mapKey={'A', '2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K'};
+
+        for(int i=0; i<mapKey.length; i++){
+            char suitKey=mapKey[i];
+            switch(suitKey){
+                case 'A':
+                    cardScore.put(suitKey, i+1); // A=1
+                    break;
+                case 'J': case 'Q': case 'K': case 'X':
+                    cardScore.put(suitKey, 10);
+                    break;
+                default:
+                    cardScore.put(suitKey, Character.getNumericValue(suitKey));
+                    break;
+            }
+        }
     }
 
     public void setValue(int n){
@@ -36,12 +56,25 @@ public class Card{
     public String getInitial(){
         return initial;
     }
+    public static HashMap<Character, Integer> getCardScore(){
+        return cardScore;
+    }
 
     public void printCurrentCardDetailed(){
         System.out.println("value is : " + this.getValue() + ", number is: " + this.getNumber() + ", suit is  : " + this.getSuit());
     }
     public void printCurrentCard(){
         System.out.printf(this.initial);
+    }
+
+    @Override
+    public int compareTo(Card card2) {
+        if(this.value==card2.value){
+            return this.suit-card2.suit;
+        }
+        else{
+            return Integer.compare(this.value, card2.value);
+        }
     }
 
     
