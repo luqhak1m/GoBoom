abc8ca0fb92a59cc27596b3973cdf7d26b14834c
NUR AISYAH NABILA <108205247+PennCakeSb@users.noreply.github.com>
2023-06-16 10:05:35 +0800

    updated save
    updated save for main deck, trick number, each player's deck, trick leaders, center deck, current player and each player's score

--- Turn.java
+++ Turn.java
@@ -1,8 +1,8 @@
-
 import java.util.Scanner;
 import java.util.TreeSet;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.io.*;
 
 public class Turn {
     mainDeck main, center;
@@ -93,8 +93,73 @@ public class Turn {
                 currentPlayerNumber=4;
             }
             players[i] = new Player(currentPlayerNumber, playersCard, i+1); // The players constructor.
+            //savePlayerDeck("player" + (i + 1) + "Deck.txt", players[i]);
         }
-    };
+    }
+
+    //save each player's deck into seperate text files
+    public void savePlayerDeck(String fileName, Player player) {
+        try {
+            FileWriter writer = new FileWriter(fileName);
+            for (Card card : player.getDeck()) {
+                String cardInfo = card.getSuit() + String.valueOf(card.getNumber());
+                writer.write(cardInfo + ", ");
+            }
+            writer.close();
+            System.out.println("Player " + player.getPlayerNum() + "'s deck saved to " + fileName);
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving the player's deck.");
+            e.printStackTrace();
+        }
+    }
+
+    //save center deck (clears file everytime deck is empty/new trick)
+    public void saveCenterDeck(String filename) {
+        try {
+            if(center.emptyDeck()){
+                PrintWriter w = new PrintWriter(filename);
+                w.print("");
+                w.close();
+            } 
+            else {
+                FileWriter w = new FileWriter(filename);
+                for (Card card : center.getDeck()){
+                    String cardInfo = card.getSuit() + String.valueOf(card.getNumber());
+                    w.write(cardInfo+"\n");
+                }
+                w.close();
+            }
+            
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving the center deck.");
+            e.printStackTrace();
+        }
+    }
+
+    //save the trick number
+    public void saveTrickNum(String filename) {
+        try {
+            FileWriter w = new FileWriter(filename);
+            String trickNumString = String.valueOf(getTrickNum());
+            w.write(trickNumString);
+            w.close();
+            System.out.println("Trick #" +getTrickNum()+" succesfully saved.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving the trick number.");
+            e.printStackTrace();
+        }
+    }
+
+    // public void saveLatestTrickLeader(String filename) {
+    //     try{
+    //         FileWriter w = new FileWriter("currentTrickLeader.txt", true);
+    //         w.write(String.valueOf(getCurrentLeadPlayer().getPlayerNum()));
+    //         w.close();
+    //     }catch (IOException e) {
+    //         System.out.println("An error occurred while saving the current trick leader.");
+    //         e.printStackTrace();
+    //     }
+    // }
     
     public void turn(Scanner input, Player player, int numOfPlayers){
         // Print round details
@@ -151,6 +216,16 @@ public class Turn {
                 printTurnDetails(numOfPlayers, center, main, player);
                 invalidButDontBreak=true;
                 break;
+                case "save":
+                main.saveMainDeck("mainDeck.txt");
+                saveTrickNum("trickNum.txt");
+                for (int i=0; i<numOfPlayers; i++){
+                    savePlayerDeck("player" + (i + 1) + "Deck.txt", players[i]);
+                }
+                player.saveTrickLeaders("trickLeaders.txt", Player.getTrickLeaders());
+                validInput=true;
+                invalidButDontBreak=true;
+                break;
             }
 
             if(userInput.length()==2 && checkPlayedCard(userInput)){
@@ -162,7 +237,9 @@ public class Turn {
                 }
             }
 
-            if(!validInput&&!invalidButDontBreak){System.out.println("Invalid input my friend.");};
+            if(!validInput&&!invalidButDontBreak) {
+                System.out.println("Invalid input my friend.");
+            }
         }
         
         if(mode==1||mode==2 || mode==3){
@@ -174,7 +251,6 @@ public class Turn {
             return;
         }
         playedCardMainMethod(player); 
-       
     }
 
     
@@ -185,6 +261,7 @@ public class Turn {
             
             center.addCard(currentPlayedCard);  
             player.getDeck().remove(currentPlayedCard);
+            saveCenterDeck("centerDeck.txt");
             
             if(currentPlayedCard.getValue()>highestValCard.getValue()){ // If the played card's value is bigger than lead card value.
             setHighestValCard(currentPlayedCard);
@@ -193,7 +270,8 @@ public class Turn {
     }
 
     public void printTurnDetails(int numOfPlayers, mainDeck center, mainDeck mainDeck, Player player){ // Construct a new turn each round
-        System.out.println(); System.out.println("Trick #" + trickNumber); // Print trick number.
+        System.out.println(); 
+        System.out.println("Trick #" + trickNumber); // Print trick number.
         System.out.println();
         
         // Print each player's deck
@@ -235,6 +313,15 @@ public class Turn {
         }
 
         System.out.println("Turn: Player " + player.getPlayerNum());
+        try{
+            PrintWriter w = new PrintWriter("currentPlayer.txt");
+            w.write(String.valueOf(player.getPlayerNum()));
+            System.out.println("Current Player saved successfully.");
+            w.close();
+        }catch(IOException e){
+            System.out.println("An error occurred while saving the current player");
+            e.printStackTrace();
+        }
         if(!center.emptyDeck()){
             System.out.println();
             showEligibleCard(player, center, mainDeck);
@@ -342,7 +429,7 @@ public class Turn {
         System.out.println();
         System.out.println("Cards will be automatically drawn if player does not have any eligible cards to play");
         System.out.println();
-        System.out.println("Commands:\nx = quit game\ns = restart game\nskip = skip turn\nhelp = show menu\ndraw = draw a single card from the main deck\ndetails = show turn details");
+        System.out.println("Commands:\nx = quit game\ns = restart game\nskip = skip turn\nhelp = show menu\ndraw = draw a single card from the main deck\ndetails = show turn details\nsave = save the game");
         System.out.println();
     }
     
@@ -361,8 +448,6 @@ public class Turn {
         Turn.Menu();
         System.out.print("Press enter to play: ");
         String command = input.nextLine();
-
-
     }
 
     public void countScore(Player player){
@@ -371,6 +456,17 @@ public class Turn {
             char num=c.getNumber();
             player.setPlayersScore(cardScore.get(num));
         }
-        System.out.println("Player " + player.getPlayerNum() + "'s score is " + player.getPlayerScore());
+        System.out.println(player.getPlayerScore());
+        //System.out.println("Player " + player.getPlayerNum() + "'s score is " + player.getPlayerScore());
+
+        try {
+            FileWriter writer = new FileWriter("playerScores.txt", true);
+            writer.write("Player " + player.getPlayerNum() + "'s score: " + player.getPlayerScore() + "\n");
+            writer.close();
+            System.out.println("Player " + player.getPlayerNum() + "'s score saved.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving the player's score.");
+            e.printStackTrace();
+        }
     }
 }
